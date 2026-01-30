e99b30f901c71f14457075cbccb624fcdfd60dbd
Bambang Tri Rahmat Doni <bambang.trd17@gmail.com>
2026-01-30 15:56:45 +0700

    Remove main storyboard

--- Tester One/AppDelegate.swift
+++ Tester One/AppDelegate.swift
@@ -13,6 +13,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
     func application(_: UIApplication, didFinishLaunchingWithOptions _: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.rootViewController = ViewController()
+        window?.makeKeyAndVisible()
         return true
     }
 
